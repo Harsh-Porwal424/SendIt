ad3c264ec3ced52648f209259cb20542fe3887d4
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-06-29 18:33:17 +0530

    Added top-header

---
+++ app/(dashboard)/_components/TopHeader.js
@@ -0,0 +1,16 @@
+import { UserButton } from '@clerk/nextjs'
+import { AlignJustify } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+const TopHeader = ({ toggleSidebar }) => {
+  return (
+    <div className='flex p-5 border-b items-center justify-between md:justify-end'>
+      <AlignJustify className='md:hidden cursor-pointer' onClick={toggleSidebar} />
+      <Image src='/logo.svg' height={100} width={150} alt="Logo" className='md:hidden' />
+      <UserButton />
+    </div>
+  )
+}
+
+export default TopHeader
--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,13 +1,33 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import SideNav from "./_components/SideNav";
+import TopHeader from "./_components/TopHeader";
 
 const Layout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div className="flex">
+      {/* Sidebar for medium and larger screens */}
       <div className="hidden md:flex h-full w-64 flex-col fixed inset-y-0 z-50">
         <SideNav />
       </div>
+
+      {/* Sidebar for small screens */}
+      {sidebarOpen && (
+        <div className="md:hidden fixed inset-0 z-50 bg-white">
+          <div className="h-full w-64">
+            <SideNav />
+          </div>
+        </div>
+      )}
+
       <div className="flex-1 md:ml-64">
+        <TopHeader toggleSidebar={toggleSidebar} />
         {children}
       </div>
     </div>
